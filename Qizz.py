0241aa47b101593225776643134bb6d04a37a7e2
stivenortiz <steivenortizcaro@gmail.com>
2023-11-01 17:47:29 -0500

    primer commit

--- Qizz.py
+++ Qizz.py
@@ -1,1 +1,17 @@
-hfdhdgd
+lista1 =[35,0,45,21]
+
+lista2 =[0.05,0.10,0.20,0.06]
+
+resultado=[]
+
+for i in range(len(lista1)):
+    multiplicacion = lista1[i] * lista2[i]
+    resultado.append(multiplicacion)
+print(resultado)
+suma = 0
+for k in resultado:
+    
+    suma += k
+
+print("suma de los valores de la lista para sacar el promedio", suma)
+
